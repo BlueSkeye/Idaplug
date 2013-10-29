162977197a2f8bae578f4e732ab162878e02d636
Blue Skeye <bsgithub@gmail.com>
2013-10-29 14:14:57 +0100

    VSIX project initial commit

---
+++ IDACsVSIX/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IDACsVSIX")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("A Visual Studio Integration package for C# IDA plugin development.")]
+[assembly: AssemblyCompany("ACME")]
+[assembly: AssemblyProduct("IDACsVSIX")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.*")]
