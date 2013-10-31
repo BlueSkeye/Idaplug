14e102be9a2600dc1086e36e7b48e01a810c905d
Blue Skeye <bsgithub@gmail.com>
2013-10-31 11:45:46 +0100

    Added a base exception class and some constants.

---
+++ IdaNet/Constants.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+// Those aliases should be copied in every source file because C# doesn't support
+// source code inclusion.
+#if __EA64__
+using AddressDifference = System.Int64;
+using EffectiveAddress = System.UInt64;
+using MemoryChunkSize = System.UInt64;
+using NodeIndex = System.UInt64;
+using SegmentSelector = System.UInt64;
+using SignedSize = System.Int64;
+#else
+using AddressDifference = System.Int32;
+using EffectiveAddress = System.UInt32;
+using MemoryChunkSize = System.UInt32;
+using NodeIndex = System.UInt32;
+using SegmentSelector = System.UInt32;
+using SignedSize = System.Int32;
+#endif
+
+namespace IdaNet
+{
+    internal static class Constants
+    {
+#if __EA64__
+        internal const string IdaDllName = "ida64.wll";
+#else
+        internal const string IdaDllName = "ida.wll";
+#endif
+    }
+}
---
+++ IdaNet/IdaNetException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdaNet
+{
+    public class IdaNetException : ApplicationException
+    {
+        #region CONSTRUCTORS
+        internal IdaNetException()
+        {
+            return;
+        }
+
+        internal IdaNetException(string message)
+            : base(message)
+        {
+            return;
+        }
+
+        internal IdaNetException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+            return;
+        }
+        #endregion
+    }
+}
